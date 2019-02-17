efc1f33fb413c5bec9566775ff185c2b9fc3160d
doomy <alexanderpaullozada@gmail.com>
2019-02-17 14:28:03 -0600

    init

---
+++ initialize.ps1
@@ -0,0 +1,24 @@
+function Remove-Directories {
+    Param ([String[]] $Directories)
+    foreach ($Directory in $Directories) {
+        Remove-Item $Directory -Recurse -Force -ErrorAction SilentlyContinue
+    }
+}
+
+function Clone-Repos {
+    Param ([String[]] $Repos, [String] $Directory)
+    New-Item $Directory -ItemType Directory -Force -ErrorAction SilentlyContinue
+    Set-Location $Directory
+    foreach ($Repo in $Repos) {
+        Invoke-Expression "git clone --recurse-submodules -j8 https://github.com/U9H/${Repo}"
+    }
+    Set-Location "../"
+}
+
+$Repos = "blog", "front"
+
+"Cleaning up workspace..."
+Remove-Directories -Directories "sites"
+
+"Cloning repositories"
+Clone-Repos "front", "blog" -Directory "sites"
